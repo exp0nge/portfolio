ea4cb83e798ec819cf3676a80f3fb5903c37127d
MD R. Islam <mdislamwork@gmail.com>
2015-07-07 21:55:47 +0000

    Adding login support TODO: register support.

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -11,7 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-
+'''
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+'''
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+
+application = get_wsgi_application()
