e8e44b4cd311a4f5b7a8a5498d7e9bdd1a67f23a
MD R. Islam <mdislamwork@gmail.com>
2015-07-07 21:09:53 +0000

    Adding tracker app.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^myportfolio/', include('myportfolio.urls'))
+    url(r'^myportfolio/', include('myportfolio.urls')),
+    url(r'^tracker/', include('tracker.urls'))
 ]
