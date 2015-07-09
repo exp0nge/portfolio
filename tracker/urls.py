1b621a7346af852ab56639ab6c629ed99bbc6284
exp0nge <mdislamwork@gmail.com>
2015-07-08 22:39:30 -0600

    Adding delete button functionality

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,5 +5,6 @@ from tracker import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add/', views.add_series, name='add_series'),
-    url(r'^delete/(?P<pk>\d+)/$', views.delete_series, name='delete_series')
+    url(r'^delete/(?P<pk>\d+)/$', views.delete_series, name='delete_series'),
+
 ]
