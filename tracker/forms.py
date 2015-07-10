b1b0b440d4a5378e87ed5a1c0f12489a3b00f631
exp0nge <mdislamwork@gmail.com>
2015-07-10 00:00:29 +0000

    Fixing add series and adding icons.

--- tracker/forms.py
+++ tracker/forms.py
@@ -7,3 +7,10 @@ class SeriesForm(forms.ModelForm):
     class Meta:
         model = Series
         fields = ('title', 'description', 'release_day', 'current_episode', 'stream_site', 'cover_image_url', 'tag')
+    
+    def __init__(self, *args, **kwargs):
+        super(SeriesForm, self).__init__(*args, **kwargs)
+        
+        for field in self.fields.values():
+            field.error_messages = {'required':'{fieldname} is required.'.format(
+                fieldname=field.label.upper())}
