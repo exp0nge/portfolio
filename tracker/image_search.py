eb0002c8b90090455125b58ef30fed9e1ef463c4
exp0nge <mdislamwork@gmail.com>
2015-07-17 21:52:51 +0000

    Changing button color scheme.

--- tracker/image_search.py
+++ tracker/image_search.py
@@ -5,6 +5,23 @@ from ipware.ip import get_ip
 import simplejson
 
 
+class NoResultError(Exception):
+    pass
+
+
+def check_blacklist(index, deserialized_output):
+        result_url = deserialized_output['responseData']['results'][index]['unescapedUrl']
+        
+        
+        if index < 5:
+            if 'imdb' in result_url:
+                check_blacklist(index + 1, deserialized_output)
+            else:
+                return deserialized_output['responseData']['results'][index]['unescapedUrl']
+        else:
+            raise NoResultError('No results found')
+
+
 def search(request, query):
     fetcher = urllib2.build_opener()
     query = urllib.quote(query)
@@ -13,4 +30,6 @@ def search(request, query):
                  '&start=0&imgsz=large&safe=active&userip=' + user_ip
     f = fetcher.open(search_url)
     deserialized_output = simplejson.load(f)
+    
+    # check_blacklist(0, deserialized_output)
     return deserialized_output['responseData']['results'][0]['unescapedUrl']
